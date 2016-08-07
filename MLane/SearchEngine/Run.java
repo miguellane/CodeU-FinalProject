f483b92b85b42466f05e512c7d3d508e36f91761
Miguel Lane <lanemiguel1337@gmail.com>
2016-08-06 19:37:31 -0700

    Jar Option Added

--- MLane/SearchEngine/Run.java
+++ MLane/SearchEngine/Run.java
@@ -1,12 +1,25 @@
 package SearchEngine;
 
+import Retriever.java;
+
 //import joptsimple.*;
 
 import java.io.IOException;
 
 public class Run {
 	public static void main(String[] args) throws IOException {
-//		Retriever retriever = new Retriever();
-//		retriever.runner();
+		System.out.println("test");
+		Retriever retriever = new Retriever();
+		retriever.runner();
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Run test = new Run();
+		System.out.println(retriever);
 	}
 }
+
