6029f9dc51e6c730c987fbb24ae2ab1609bcb625
Miguel Lane <lanemiguel1337@gmail.com>
2016-08-06 19:56:45 -0700

    Revert

--- MLane/SearchEngine/Run.java
+++ MLane/SearchEngine/Run.java
@@ -1,42 +1,22 @@
 package SearchEngine;
 
-import Retriever.java;
+//import Retriever.java;
 
 //import joptsimple.*;
 
 import java.io.IOException;
 
 public class Run {
-	public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
-		System.out.println("test");
-		Retriever retriever = new Retriever();
-		retriever.runner();
-	}
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
-		Run test = new Run();
-		System.out.println(retriever);
-=======
+//		Run test = new Run();
+//		System.out.println(retriever);
+
 		//System.out.println("test");
 		//Retriever retriever = new Retriever();
 		//retriever.runner();
->>>>>>> 2390ffad5ac415066890e55ebeb85697fdcf20dd
+
 	}
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	/*
-	public static void main(String[] args) throws IOException {
-		Run test = new Run();
-		System.out.println(retriever);
-	}
-	*/
 }
 
