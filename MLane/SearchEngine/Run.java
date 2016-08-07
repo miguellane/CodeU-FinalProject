2390ffad5ac415066890e55ebeb85697fdcf20dd
Zoe D <zdormuth@gmail.com>
2016-08-06 21:37:34 -0500

    Sub Branch Test!

--- MLane/SearchEngine/Run.java
+++ MLane/SearchEngine/Run.java
@@ -8,18 +8,20 @@ import java.io.IOException;
 
 public class Run {
 	public static void main(String[] args) throws IOException {
-		System.out.println("test");
-		Retriever retriever = new Retriever();
-		retriever.runner();
+		//System.out.println("test");
+		//Retriever retriever = new Retriever();
+		//retriever.runner();
 	}
 
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
+	/*
 	public static void main(String[] args) throws IOException {
 		Run test = new Run();
 		System.out.println(retriever);
 	}
+	*/
 }
 
