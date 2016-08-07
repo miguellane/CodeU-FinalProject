31b186107dacfda9207d1bc30357b2cbd57279bb
Miguel Lane <lanemiguel1337@gmail.com>
2016-08-06 22:39:02 -0700

    Added multiple word searching, non additive yet.

--- MLane/SearchEngine/Run.java
+++ MLane/SearchEngine/Run.java
@@ -13,19 +13,23 @@ import redis.clients.jedis.Jedis;
 public class Run {
 
 	public static void main(String[] args) throws IOException {
-//		int option = 0;
+		int option = 0;
 		Scanner reader = new Scanner(System.in);
-		System.out.println("Hello, please enter a search phrase:");
-//		System.out.println("Options include 'Site:', 'Cringe:', 'IFL:', 'AND', 'OR', '-'");
-		String cmdLine = reader.next();
-		String[] searches = cmdLine.split(" ");
-//		search = searches[0];
-//		if(search.compareToIgnoreCase("Cringe:");		
-		
 		Jedis jedis = JedisMaker.make();
 		JedisIndex index = new JedisIndex(jedis); 
-		
-		for(String search: searches){
+
+		System.out.println("Hello, please enter a search phrase:");
+		System.out.println("Two terms not seperated by operator will be treated as AND:");
+		System.out.println("Options include 'Site:', 'Cringe:', 'IFL:', 'AND', 'OR', '-'");
+		String cmdLine = reader.nextLine();
+		String[] searches = cmdLine.split("\\s+");
+		switch(searches[0]){
+			case "Site:":	option = 1;	break;
+			case "IFL:":	option = 2;	break;
+			case "Cringe:":	option = 3;	break;
+		}
+		for(int i = 0; i < searches.length; i++ ){
+			String search = searches[i];
 			System.out.println("Query: " + search);
 			Retriever search1 = new Retriever(search, index);
 			search1.print();
