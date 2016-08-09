e25ef89baa1369c04bae2e4f50962492ca7ff27a
Miguel Lane <lanemiguel1337@gmail.com>
2016-08-09 15:51:07 -0700

    Edits

--- MLane/SearchEngine/JedisIndex.java
+++ MLane/SearchEngine/JedisIndex.java
@@ -26,122 +26,53 @@ public class JedisIndex {
 		this.jedis = jedis;
 	}
 	
-	/**
-	 * Returns the Redis key for a given search term.
-	 * 
-	 * @return Redis key.
-	 */
 	private String urlSetKey(String term) {
 		return "URLSet:" + term;
 	}
-	
-	/**
-	 * Returns the Redis key for a URL's TermCounter.
-	 * 
-	 * @return Redis key.
-	 */
 	private String termCounterKey(String url) {
 		return "TermCounter:" + url;
 	}
 
-	/**
-	 * Checks whether we have a TermCounter for a given URL.
-	 * 
-	 * @param url
-	 * @return
-	 */
+//Function methods
 	public boolean isIndexed(String url) {
 		String redisKey = termCounterKey(url);
 		return jedis.exists(redisKey);
 	}
 	
-	/**
-	 * Adds a URL to the set associated with `term`.
-	 * 
-	 * @param term
-	 * @param tc
-	 */
 	public void add(String term, TermCounter tc) {
 		jedis.sadd(urlSetKey(term), tc.getLabel());
 	}
 
-	/**
-	 * Looks up a search term and returns a set of URLs.
-	 * 
-	 * @param term
-	 * @return Set of URLs.
-	 */
 	public Set<String> getURLs(String term) {
 		Set<String> set = jedis.smembers(urlSetKey(term));
 		return set;
 	}
 
-	/**
-	 * Looks up a term and returns a map from URL to count.
-	 * 
-	 * @param term
-	 * @return Map from URL to count.
-	 */
 	public Map<String, Integer> getCounts(String term) {
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		Set<String> urls = getURLs(term);
-		for (String url: urls) {
-			Integer count = getCount(url, term);
-			map.put(url, count);
-		}
-		return map;
-	}
-
-	/**
-	 * Looks up a term and returns a map from URL to count.
-	 * 
-	 * @param term
-	 * @return Map from URL to count.
-	 */
-	public Map<String, Integer> getCountsFaster(String term) {
-		// convert the set of strings to a list so we get the
-		// same traversal order every time
 		List<String> urls = new ArrayList<String>();
 		urls.addAll(getURLs(term));
-
-		// construct a transaction to perform all lookups
 		Transaction t = jedis.multi();
 		for (String url: urls) {
 			String redisKey = termCounterKey(url);
 			t.hget(redisKey, term);
 		}
 		List<Object> res = t.exec();
-
-		// iterate the results and make the map
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		int i = 0;
 		for (String url: urls) {
-			System.out.println(url);
+//			System.out.println(url);
 			Integer count = new Integer((String) res.get(i++));
 			map.put(url, count);
 		}
 		return map;
 	}
 
-	/**
-	 * Returns the number of times the given term appears at the given URL.
-	 * 
-	 * @param url
-	 * @param term
-	 * @return
-	 */
 	public Integer getCount(String url, String term) {
 		String redisKey = termCounterKey(url);
 		String count = jedis.hget(redisKey, term);
 		return new Integer(count);
 	}
 
-	/**
-	 * Add a page to the index.
-	 * 
-	 * @param url         URL of the page.
-	 * @param paragraphs  Collection of elements that should be indexed.
-	 */
 	public void indexPage(String url, Elements paragraphs) {
 		System.out.println("Indexing " + url);
 		
@@ -152,13 +83,6 @@ public class JedisIndex {
 		// push the contents of the TermCounter to Redis
 		pushTermCounterToRedis(tc);
 	}
-
-	/**
-	 * Pushes the contents of the TermCounter to Redis.
-	 * 
-	 * @param tc
-	 * @return List of return values from Redis.
-	 */
 	public List<Object> pushTermCounterToRedis(TermCounter tc) {
 		Transaction t = jedis.multi();
 		
@@ -178,33 +102,7 @@ public class JedisIndex {
 		List<Object> res = t.exec();
 		return res;
 	}
-
-	/**
-	 * Prints the contents of the index.
-	 * 
-	 * Should be used for development and testing, not production.
-	 */
-	public void printIndex() {
-		// loop through the search terms
-		for (String term: termSet()) {
-			System.out.println(term);
-			
-			// for each term, print the pages where it appears
-			Set<String> urls = getURLs(term);
-			for (String url: urls) {
-				Integer count = getCount(url, term);
-				System.out.println("    " + url + " " + count);
-			}
-		}
-	}
-
-	/**
-	 * Returns the set of terms that have been indexed.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
+//Testing methods.
 	public Set<String> termSet() {
 		Set<String> keys = urlSetKeys();
 		Set<String> terms = new HashSet<String>();
@@ -218,36 +116,16 @@ public class JedisIndex {
 		}
 		return terms;
 	}
-
-	/**
-	 * Returns URLSet keys for the terms that have been indexed.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
 	public Set<String> urlSetKeys() {
 		return jedis.keys("URLSet:*");
 	}
-
-	/**
-	 * Returns TermCounter keys for the URLS that have been indexed.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
 	public Set<String> termCounterKeys() {
 		return jedis.keys("TermCounter:*");
 	}
-
-	/**
-	 * Deletes all URLSet objects from the database.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
+	public int urlNum(){
+//		return jedis.llen("TermCounter:");
+		return termCounterKeys().size();
+ 	}
 	public void deleteURLSets() {
 		Set<String> keys = urlSetKeys();
 		Transaction t = jedis.multi();
@@ -256,14 +134,6 @@ public class JedisIndex {
 		}
 		t.exec();
 	}
-
-	/**
-	 * Deletes all URLSet objects from the database.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
 	public void deleteTermCounters() {
 		Set<String> keys = termCounterKeys();
 		Transaction t = jedis.multi();
@@ -272,14 +142,6 @@ public class JedisIndex {
 		}
 		t.exec();
 	}
-
-	/**
-	 * Deletes all keys from the database.
-	 * 
-	 * Should be used for development and testing, not production.
-	 * 
-	 * @return
-	 */
 	public void deleteAllKeys() {
 		Set<String> keys = jedis.keys("*");
 		Transaction t = jedis.multi();
